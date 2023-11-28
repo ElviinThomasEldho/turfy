cd0bfb07b0f24acc9354b92cc845c467998aba74
Gayathry Bijoy <gayathrybijoy@gmail.com>
2023-11-28 15:10:48 +0530

    updated models

--- app/models.py
+++ app/models.py
@@ -16,33 +16,26 @@ class Player(models.Model):
 
     def __str__(self):
         return str(self.id) + " | " + self.firstName + " " + self.lastName 
-
-
-class Payment(models.Model):
-
-    player = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    turf = models.ForeignKey(Turf, null=True, on_delete=models.SET_NULL)
     
-<<<<<<< HEAD
+class Days(models.Model):
+    days = models.CharField("Day of the week", max_length=25, null=True)
     
-    mode
-
-
-
-
-
-=======
->>>>>>> e43b5db55e9a5757540bde1b889852f5a45f92bc
+class TimeSlot(models.Model): 
+    CHOICE = (
+        ("AM","AM")
+        ("PM","PM")
+    )
+    startTime = models.IntegerField("Start Time", max_length=2,null=True)
+    choice = models.CharField("Choice",max_length=3,choices=CHOICE)
+    endTime =models.IntegerField("End Time", max_length=2,null=True)
+    choice = models.CharField("Choice",max_length=3,choices=CHOICE)
+    days= models.ManyToManyField(Days, null=True, blank=True) 
 
 class Turf(models.Model):
     TYPE =  (
         ('5s','5s'),
         ('7s','7s')
     )
-<<<<<<< HEAD
-=======
-
->>>>>>> e43b5db55e9a5757540bde1b889852f5a45f92bc
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     ownerName = models.CharField("First Name", max_length=25, null=True)
@@ -50,19 +43,33 @@ class Turf(models.Model):
     location = models.CharField("Location ", max_length=200, null=True)
     phoneNumber = models.CharField("Phone Number", max_length=25, null=True)
     pricePerSlot = models.FloatField("Price Per Slot", max_length=3, null=True)
-<<<<<<< HEAD
     fieldSize = models.CharField("fieldSize",max_length=3,choices=TYPE )
-    
-    
+    slots = models.ManyToManyField(TimeSlot, null=True, blank=True) 
+
     def __str__(self):
         return str(self.id) + " | " + self.turfName + " | " + self.ownerName 
-=======
-    type = models.CharField("Type", max_length=25, choices=TYPE, null=True)
     
-    slots = models.ManyToManyField(TimeSlot, null=True, blank=True)
-    
-    def __str__(self):
-        return str(self.id) + " | " + self.turfName + " | " + self.ownerName 
+
+class Booking(models.Model):
+    player = models.ForeignKey(Player, null=True, on_delete=models.SET_NULL)
+    turf = models.ForeignKey(Turf, null=True, on_delete=models.SET_NULL)
+    timeSlot = models.ForeignKey(TimeSlot, null=True, on_delete=models.SET_NULL)
+
+    date = models.DateField("Date", null=True)
+    noOfParticipants = models.IntegerField("No of Participants",max_length=2,null=True)
 
 
->>>>>>> e43b5db55e9a5757540bde1b889852f5a45f92bc
+
+class Payment(models.Model):
+    MODE =(
+        ("UPI","UPI")
+        ("Cash","Cash")
+    )
+    player = models.ForeignKey(Player, null=True, on_delete=models.SET_NULL)
+    turf = models.ForeignKey(Turf, null=True, on_delete=models.SET_NULL)
+    booking = models.ForeignKey(Booking, null=True, on_delete=models.SET_NULL)
+
+    mode = models.CharField("Payment Mode",max_length=3,choices=MODE)
+    screenshot = models.ImageField("Payment Screenshot Upload",null=True)
+    amount = models.FloatField("Toatl Amount ", max_length= 10, null = True)
+
